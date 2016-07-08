03afe8eab0ebbb11d2d9767efc802b8110696577
Piper Merriam <pipermerriam@gmail.com>
2016-07-07 23:59:16 -0600

    enable CI testing using travis and tox

--- func_sig_registry/settings.py
+++ func_sig_registry/settings.py
@@ -95,12 +95,6 @@ WSGI_APPLICATION = 'func_sig_registry.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 DATABASES = {}
 
 DATABASES['default'] = dj_database_url.config(
