aa528f22a01973cd9d6e134d62e7ab1ec8d8fe3c
Piper Merriam <pipermerriam@gmail.com>
2016-07-08 23:17:39 -0600

    dirty

---
+++ tests/utility/test_make_4byte_signature.py
@@ -0,0 +1,18 @@
+import pytest
+
+from func_sig_registry.utils.encoding import (
+    encode_hex,
+    force_text,
+)
+from func_sig_registry.utils.abi import make_4byte_signature
+
+
+@pytest.mark.parametrize(
+    'text_signature,hex_signature',
+    (
+        ('isCancellable()', '0x4500054f'),
+    ),
+)
+def test_make_4byte_signature(factories, text_signature, hex_signature):
+    signature = factories.SignatureFactory(text_signature=text_signature)
+    assert hex_signature == signature.bytes_signature.get_hex_display()
