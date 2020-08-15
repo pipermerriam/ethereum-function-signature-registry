cb3b8e2e02ce745d670c41f552c6b1656b3806f1
Nurtas <inurtas1999@gmail.com>
2020-08-16 02:43:57 +0600

    rollback settings_test.py

--- func_sig_registry/settings_test.py
+++ func_sig_registry/settings_test.py
@@ -7,8 +7,7 @@ os.environ.setdefault('DJANGO_SECURE_SSL_REDIRECT', 'False')
 os.environ.setdefault('REDIS_URL', 'redis://localhost:6379')
 os.environ.setdefault(
     'DATABASE_URL',
-    'postgres://postgres:postgres@localhost/func_sig_registry',
-    # 'postgres://{user}@localhost/func_sig_registry'.format(user=getpass.getuser()),
+    'postgres://{user}@localhost/func_sig_registry'.format(user=getpass.getuser()),
 )
 os.environ.setdefault('ROLLBAR_ACCESS_TOKEN', 'not-a-real-access-token')
 os.environ.setdefault('ROLLBAR_ENVIRONMENT', 'test')
