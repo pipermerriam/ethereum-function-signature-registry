341f7b7155f1cb659e90ad397860cb8464dd5c51
Ayan Ginet <ayan.ginet@nu.edu.kz>
2020-08-17 18:41:19 +0500

    implements event signature table

--- func_sig_registry/registry/tables.py
+++ func_sig_registry/registry/tables.py
@@ -2,6 +2,7 @@ import django_tables2 as tables
 
 from .models import (
     Signature,
+    EventSignature,
 )
 
 
@@ -16,6 +17,20 @@ class SignatureTable(tables.Table):
             'id',
             'text_signature',
             'bytes_signature',
+        )
+        template = 'partials/table.html'
+        attrs = {
+            'class': 'table table-striped table-bordered',
+        }
+
+
+class EventSignatureTable(tables.Table):
+    class Meta:
+        model = EventSignature
+        fields = (
+            'id',
+            'text_signature'
+            'hex_signature'
         )
         template = 'partials/table.html'
         attrs = {
