db22ed29683af8c588efa018279b7da027fbc913
Ayan Ginet <ayan.ginet@nu.edu.kz>
2020-08-18 21:27:47 +0500

    adds test for solidity code & file simultaneous import

---
+++ tests/web/api/test_solidity_code_and_file_import.py
@@ -0,0 +1,40 @@
+from io import BytesIO
+from rest_framework import status
+from django.core.urlresolvers import reverse
+
+
+CODE = """
+contract Foo {
+    function foo_1(uint a) {
+    }
+
+    event foo_2(uint a);
+}
+"""
+
+FILE = b"""
+contract Bar {
+    function bar_1(uint a) {
+
+    }
+
+    event bar_2(uint a);
+}
+"""
+
+
+def test_importing_solidity_source_code(api_client, factories):
+    import_url = reverse('api:import-solidity')
+    source_file = BytesIO(FILE)
+
+    response = api_client.post(import_url, {
+        'source_code': CODE,
+        'source_fuke': source_file,
+    }, format='multipart')
+
+    assert response.status_code == status.HTTP_201_CREATED
+    data = response.data
+    assert data['num_processed'] == 4
+    assert data['num_imported'] == 4
+    assert data['num_duplicates'] == 0
+    assert data['num_ignored'] == 0
