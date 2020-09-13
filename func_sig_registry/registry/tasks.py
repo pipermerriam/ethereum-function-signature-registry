12be4a3d016295c30288dab5cf81dc54c585856e
Nurtas <inurtas1999@gmail.com>
2020-09-14 01:03:28 +0600

    Update perform_github_import

--- func_sig_registry/registry/tasks.py
+++ func_sig_registry/registry/tasks.py
@@ -3,8 +3,13 @@ import sys
 
 from huey.contrib.djhuey import db_task
 
-from .models import Signature
-
+from .models import (
+    Signature,
+    EventSignature,
+)
+from func_sig_registry.utils.github import (
+    get_repository_solidity_files,
+)
 
 logger = logging.getLogger('bytes4.github_import')
 logger.setLevel(logging.INFO)
@@ -18,5 +23,14 @@ logger.addHandler(ch)
 
 @db_task()
 def perform_github_import(login_or_name, repository, branch):
-    logger.info("Importing github repo %s/%s/%s", login_or_name, repository, branch)
-    Signature.import_from_github_repository(login_or_name, repository, branch)
+    logger.info("Importing github repo %s/%s/%s", login_or_name,
+                repository, branch)
+    for file_path in get_repository_solidity_files(login_or_name, repository, branch):
+            logger.info("importing solidity file: %s", file_path)
+            with open(file_path) as solidity_file:
+                try:
+                    Signature.import_from_solidity_file(solidity_file)
+                    EventSignature.import_from_solidity_file(solidity_file)
+                except UnicodeDecodeError:
+                    logger.error('unicode error reading solidity file: %s',
+                                 file_path)
