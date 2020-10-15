8ef570c9ee47300a6d3e3eaa5185f1219d166253
Ayan Ginet <ayan.ginet@nu.edu.kz>
2020-10-15 14:14:30 +0500

    additional docker image for postgres

---
+++ setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+extras_require = {
+    'test': [
+        "pytest==5.4.1",
+        "pytest-xdist",
+        "tox==3.14.6",
+    ],
+    'lint': [
+        "flake8==3.7.9",
+        "isort>=4.2.15,<5",
+        "mypy==0.770",
+        "pydocstyle>=3.0.0,<4",
+    ],
+    'doc': [
+        "Sphinx>=1.6.5,<2",
+        "sphinx_rtd_theme>=0.1.9",
+        "towncrier>=19.2.0, <20",
+    ],
+    'dev': [
+        "bumpversion>=0.5.3,<1",
+        "pytest-watch>=4.1.0,<5",
+        "wheel",
+        "twine",
+        "ipython",
+    ],
+}
+
+extras_require['dev'] = (
+    extras_require['dev'] +  # noqa: W504
+    extras_require['test'] +  # noqa: W504
+    extras_require['lint'] +  # noqa: W504
+    extras_require['doc']
+)
+
+
+with open('./README.md') as readme:
+    long_description = readme.read()
+
+
+setup(
+    name='<PYPI_NAME>',
+    # *IMPORTANT*: Don't manually change the version here. Use `make bump`, as described in readme
+    version='0.1.0-alpha.0',
+    description="""<PYPI_NAME>: <SHORT_DESCRIPTION>""",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='The Ethereum Foundation',
+    author_email='snakecharmers@ethereum.org',
+    url='https://github.com/ethereum/<REPO_NAME>',
+    include_package_data=True,
+    install_requires=[
+        "eth-utils>=1,<2",
+    ],
+    python_requires='>=3.6, <4',
+    extras_require=extras_require,
+    py_modules=['<MODULE_NAME>'],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
